13c038786d735a8097c78a8f8a9e8688d0471526
NDevOps.Desktop <ndevops.desktop@shaw.ca>
2018-12-09 20:52:02 -0800

    MasterFrame, Git configuration added.

---
+++ src/com/ndevopssolution/view/MasterFrame.java
@@ -0,0 +1,51 @@
+package com.ndevopssolution.view;
+
+import java.awt.BorderLayout;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * 
+ * @FileName MasterFrame.java
+ *
+ * @Project Name JLC-System
+ * @author NDevOps
+ * 
+ * @Created on Dec/09/2018
+ */
+
+public class MasterFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Default constructor
+	 */
+	public MasterFrame() {
+		super("Jesus Lighthouse Church");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getSize());
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setResizeWeight(0.2);
+		contentPane.add(splitPane, BorderLayout.CENTER);
+	}
+	/**
+	 * @param args - arguments to be pass when run in commandline
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MasterFrame frame = new MasterFrame();
+		frame.setVisible(true);
+	}
+
+}
