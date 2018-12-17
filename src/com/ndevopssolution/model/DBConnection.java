ab4cdfc0ff146bad764e23311448655af3eaa2b4
NDevOps.Desktop <ndevops.desktop@shaw.ca>
2018-12-16 22:49:40 -0800

    access database added.

---
+++ src/com/ndevopssolution/model/DBConnection.java
@@ -0,0 +1,18 @@
+package com.ndevopssolution.model;
+
+/**
+ * 
+ * @FileName DBConnection.java
+ *
+ * @Project Name JLC-System
+ * @author NDevOps
+ * 
+ * @Created on Dec/16/2018
+ */
+public class DBConnection {
+	
+	public DBConnection() {
+		
+	}
+	
+}
